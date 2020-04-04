3e9fa09976d33666ace38d9e394493b0411e55eb
Mark Doyle <markdoyle222@gmail.com>
2020-04-05 00:29:29 +0100

    refactor circle component

--- src/components/GraphComponents/Circle.vue
+++ src/components/GraphComponents/Circle.vue
@@ -2,31 +2,31 @@
   <div class="screen">
     <div class="dials" :style="{width: halfRadiusEm, height: halfRadiusEm}">
       <div class="dial move" :style="[cssVars,{width: radiusEm, height: radiusEm}]">
-        <div class="dial-background one"></div>
+        <div class="rail-background"></div>
         <div class="dial-container container1" :style="{width: halfRadiusEm, left: halfRadiusEm}">
           <div
-            class="wedge"
-            :style="{
+            class="half-circle"
+            :style="[circleDegrees,{
               width: halfRadiusEm,
               height: radiusEm,
               left: `-${halfRadiusEm}`,
               borderRadius: `${radiusEm} 0 0 ${radiusEm}`,
-            }"
+            }]"
           />
         </div>
         <div class="dial-container container2" :style="{width: halfRadiusEm, left: 0}">
           <div
-            class="wedge"
-            :style="{
+            class="half-circle"
+            :style="[circleDegrees,{
               width: halfRadiusEm,
               height: radiusEm,
               borderRadius: `0 ${radiusEm} ${radiusEm} 0`,
               left: halfRadiusEm,
-            }"
+            }]"
           />
         </div>
-        <div class="marker start"></div>
-        <div class="marker end" :style="{transformOrigin: `50% ${halfRadiusEm}`}"></div>
+        <div class="marker"></div>
+        <div class="marker end" :style="[circleDegrees, {transformOrigin: `50% ${halfRadiusEm}`}]"></div>
       </div>
     </div>
   </div>
@@ -35,6 +35,10 @@
 <script>
 export default {
   props: {
+    percentage: {
+      type: Number,
+      default: 65,
+    },
     background: {
       type: String,
       default: '#32CBD4',
@@ -45,23 +49,45 @@ export default {
     },
     centerColor: {
       type: String,
-      default: '#fff',
+      default: '#030303',
     },
     radius: {
       type: Number,
-      default: 8,
-    },
-    centerSize: {
-      type: Number,
-      default: 6,
+      default: 7,
     },
   },
   computed: {
     cssVars() {
       return {
-        '--centerSize': `${this.centerSize}em`,
+        '--centerSize': `${this.radius - 2}em`,
         '--backgroundColor': this.background,
         '--centerColor': this.centerColor,
+        '--railColor': this.railColor,
+      };
+    },
+    circleDegrees() {
+      const percentage = this.percentage / 100;
+      const totalDegrees = percentage * 360;
+      let firstCircRadius = Number;
+      let secondCircRadius = Number;
+      let secondCircTiming = 0;
+      let endTiming = 1;
+
+      if (totalDegrees > 180) {
+        firstCircRadius = 180;
+        secondCircRadius = totalDegrees - 180;
+        secondCircTiming = secondCircRadius * 0.005555; // 180-sec = 1 degree every 0.005* seconds
+        endTiming = 1 + secondCircTiming;
+      } else {
+        firstCircRadius = totalDegrees;
+        secondCircRadius = 0;
+      }
+      return {
+        '--firstCircDeg': `${firstCircRadius}deg`,
+        '--secondCircDeg': `${secondCircRadius}deg`,
+        '--secondCircTiming': `${secondCircTiming}s`,
+        '--endTiming': `${endTiming}s`,
+        '--totalDeg': `${totalDegrees}deg`,
       };
     },
     halfRadiusEm() {
@@ -79,17 +105,17 @@ export default {
   box-sizing: content-box;
 }
 
-.screen {
-  margin-bottom: 2rem;
+.container {
   position: relative;
-  background: white;
 
-  .dials {
-    animation: show-dials 5s infinite ease-out;
-    border-radius: 50%;
+  .rail-background {
     position: absolute;
-    top: 1.75em;
+    top: 0;
+    right: 0;
+    bottom: 0;
     left: 0;
+    border-radius: 50%;
+    background-color: var(--railColor);
   }
 
   .dial {
@@ -118,20 +144,23 @@ export default {
       bottom: 0;
       overflow: hidden;
     }
-    .wedge {
+
+    .half-circle {
       position: absolute;
       top: 0;
       left: 0;
     }
 
-    .container1 .wedge {
-      animation: rotate-bg-dial-1-1 5s 0.2s linear infinite;
+    .container1 .half-circle {
+      animation: rotate-bg-dial-1-1 1s linear forwards;
       background: var(--backgroundColor);
       transform-origin: 100% 50%;
     }
 
-    .container2 .wedge {
-      animation: rotate-bg-dial-1-2 5s 0.2s linear infinite;
+    .container2 .half-circle {
+      animation: rotate-bg-dial-1-2 linear forwards;
+      animation-delay: 1s;
+      animation-duration: var(--secondCircTiming);
       background: var(--backgroundColor);
       transform-origin: 0 50%;
     }
@@ -146,7 +175,8 @@ export default {
       left: calc(50% - 0.5em);
     }
     .end {
-      animation: rotate-marker-dial-1 5s 0.2s linear infinite;
+      animation: rotate-marker-dial-1 linear forwards;
+      animation-duration: var(--endTiming);
     }
   }
 }
@@ -154,46 +184,29 @@ export default {
 /** Animations **/
 
 @keyframes rotate-marker-dial-1 {
-  0%,
-  6% {
+  0% {
     transform: none;
   }
-  28%,
-  94% {
-    transform: rotateZ(220deg);
-  }
-  95%,
   100% {
-    transform: none;
+    transform: rotateZ(var(--totalDeg));
   }
 }
 
 @keyframes rotate-bg-dial-1-1 {
-  0%,
-  6% {
+  0% {
     transform: rotateZ(0);
   }
-  24%,
-  94% {
-    transform: rotateZ(180deg);
-  }
-  95%,
   100% {
-    transform: rotateZ(0);
+    transform: rotateZ(var(--firstCircDeg));
   }
 }
 
 @keyframes rotate-bg-dial-1-2 {
-  24% {
+  0% {
     transform: rotateZ(0);
   }
-  28%,
-  94% {
-    transform: rotateZ(40deg);
-  }
-  95%,
   100% {
-    transform: rotateZ(0);
+    transform: rotateZ(var(--secondCircDeg));
   }
 }
 </style>
--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,21 +1,7 @@
 <template>
-  <div class="Weather">
-    <HalfCircle />
-  </div>
+  <div class="Weather"></div>
 </template>
 
 <script>
-import HalfCircle from '../components/GraphComponents/HalfCircle.vue';
 
-export default {
-  components: { HalfCircle },
-};
 </script>
-
-<style lang="scss">
-.Weather {
-  height: 100vh;
-  width: 100vw;
-  background-color: white;
-}
-</style>
