9c9bd6230f33bd26893fd37e61396d692ea3e172
mmaddox93 <mmaddox2084@gmail.com>
2022-02-12 09:43:15 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'http://vatsim.siliconrp.com' > CNAME
+#echo 'http://vatsim.siliconrp.com' > CNAME
 
 git init
 git add -A
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:mmaddox93/Vatsim-Live.git main:gh-pages
 
 cd -
